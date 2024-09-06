2ebb5237eee633b1e2dcccfdad49f93179958258
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-09-06 15:45:21 -0300

    mapa do senai

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import { Card2 } from './components/Card2'
 import produtos from './constants/produtos.json'
 import { api } from "./api/rmApi"
 import style from './App.module.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import "leaflet-defaulticon-compatibility";
+
 
 function App() {
   const [show, setShow] = useState("")
@@ -87,7 +92,17 @@ function App() {
         <>
       <h2>Mapa</h2>
           <div>
-              mapa aqui
+          <MapContainer center={[-25.4249647,-49.272303]} zoom={13} scrollWheelZoom={true} style={{width:"100%", height: '400px'}}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-25.4249647,-49.272303]}>
+            <Popup>
+             <a href="https://www.google.com.br/maps/place/R.+Paula+Gomes,+270+-+Centro,+Curitiba+-+PR,+80510-030/@-25.4249647,-49.272303,17z/data=!4m6!3m5!1s0x94dce5795befda7d:0xfd4f054127f9a778!8m2!3d-25.4249647!4d-49.272303!16s%2Fg%2F11krkw3wp0?hl=pt-BR&entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D" target='_blank'>Senai Celso Charuri</a>
+            </Popup>
+          </Marker>
+        </MapContainer>
           </div>
          </>
       }
