3d8422a2c66740f6c94c677b7100224c73a0fb90
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-09-12 16:43:43 -0300

    botao de fechar

--- src/components/ModalInfo.jsx
+++ src/components/ModalInfo.jsx
@@ -10,6 +10,7 @@ const ModalInfo = (item) => {
        <div key={item.data.id} className={style.Card3}>
                 <Card2 name={item.data.name} species={item.data.species} gender={item.data.gender} type={item.data.type} status={item.data.status} image={item.data.image}/>
             
+       <button onClick={item.close}>&#128473;</button>
               </div>
         </Draggable>
     )
